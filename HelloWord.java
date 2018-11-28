f61835d5143f69118dc41ee0591121f86702a1dc
Sally Mogala <mogalas@hq.za.ovationsgroup.com>
2018-11-28 12:09:00 +0200

    css

---
+++ HelloWord.java
@@ -0,0 +1,19 @@
+public class HelloWord
+{
+	
+	public static void main(String [] arg)
+	{
+		int age=10;
+		int one=1;
+		int sum= age + one;
+		String name="Sally";
+		
+		
+		
+		
+		System.out.println(name +" is" +" "+ age +" years old.");
+		System.out.println("2019 she will be "+ sum + " years old");
+		
+	}
+	
+}
